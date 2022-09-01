c61941fbcdf5430be04ca500c949fc066e6f599d
MaksimGoleota <74358982+maksimgoleota@users.noreply.github.com>
2022-09-01 19:45:54 +0300

    basic setup: 1. Adding Libraries. 2. Create folders. 3. Create ApiModule and NetworkModule. 4. Create RequestInterceptor. etc...

--- app/src/main/java/com/merio/visualparadise/MainActivity.kt
+++ app/src/main/java/com/merio/visualparadise/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.merio.visualparadise
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.merio.visualparadise.example.UjunaldAdapter
+import com.merio.visualparadise.example.UjunaldViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var ujunaldList: RecyclerView
+    private val mViewModel: UjunaldViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        ujunaldList = findViewById<RecyclerView>(R.id.cryptocurrency_list)
+        ujunaldList.layoutManager = LinearLayoutManager(this)
+
+        observeUjunald()
+    }
+
+    private fun observeUjunald() {
+        mViewModel.ujunaldEmitter.observe(this) {
+            ujunaldList.adapter = UjunaldAdapter(it)
+        }
     }
 }
