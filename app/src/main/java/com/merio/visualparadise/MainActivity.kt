caaef2f011ca1ea68854721f82a54c67680c4986
MaksimGoleota <74358982+maksimgoleota@users.noreply.github.com>
2022-09-06 09:31:00 +0300

    Creating the first search screen. Navigation setup. Creating an app icon...

--- app/src/main/java/com/merio/visualparadise/MainActivity.kt
+++ app/src/main/java/com/merio/visualparadise/MainActivity.kt
@@ -3,6 +3,8 @@ package com.merio.visualparadise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.merio.visualparadise.example.UjunaldAdapter
@@ -12,23 +14,31 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var ujunaldList: RecyclerView
-    private val mViewModel: UjunaldViewModel by viewModels()
+    private val navController: NavController by lazy {
+        findNavController(R.id.nav_fragment)
+    }
+
+//    private lateinit var ujunaldList: RecyclerView
+//    private val mViewModel: UjunaldViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val graph = navController.navInflater.inflate(R.navigation.navigation)
+        navController.graph = graph
 
-        ujunaldList = findViewById<RecyclerView>(R.id.cryptocurrency_list)
-        ujunaldList.layoutManager = LinearLayoutManager(this)
-
-        observeUjunald()
+//        ujunaldList = findViewById<RecyclerView>(R.id.cryptocurrency_list)
+//        ujunaldList.layoutManager = LinearLayoutManager(this)
+//
+//        observeUjunald()
     }
 
-    private fun observeUjunald() {
-        mViewModel.ujunaldEmitter.observe(this) {
-            ujunaldList.adapter = UjunaldAdapter(it)
-        }
-    }
+//    private fun observeUjunald() {
+//        mViewModel.ujunaldEmitter.observe(this) {
+//            ujunaldList.adapter = UjunaldAdapter(it)
+//        }
+//    }
+
+
 }
--- app/src/main/java/com/merio/visualparadise/data/di/ApiModule.kt
+++ app/src/main/java/com/merio/visualparadise/data/di/ApiModule.kt
@@ -3,9 +3,12 @@ package com.merio.visualparadise.data.di
 import com.merio.visualparadise.data.network.api.VisualParadiseApiClient
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 
 @Module
+@InstallIn(SingletonComponent::class)
 class ApiModule {
 
     @Provides
